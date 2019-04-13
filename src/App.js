74a2ffb3195e33062560eeb68bd37f2e02e6a082
bhupeshbhatia <bhupesh.bhatia@gmail.com>
2019-04-13 17:01:45 -0400

    Creating erc721 token hash

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+//===================================================================================
+import { MetamaskProvider } from "@0xcert/ethereum-metamask-provider";
+import { schema88 } from "@0xcert/conventions";
+import { Cert } from "@0xcert/cert";
+import {
+  AssetLedger,
+  AssetLedgerCapability
+} from "@0xcert/ethereum-asset-ledger";
+// Assets Ledgers are groups of tokens that are managed by certain users just like mods in a chat to do what's required
+// The Capabilities determine what those mods can do with the assets they are managing
+// The Ethereum address that deploys this ledger has full powers to do whatever he wants as the administrator
+
+const path = require("path");
+const fs = require("fs");
+
+// To configure new ERC721 assets
+async function displayBlueprint() {
+  const cert = new Cert({
+    schema: schema88
+  });
+
+  const asset = {
+    name: "Unsplash",
+    description: "Photo by Madison Olling on Unsplash",
+    image: fs.readFileSync(
+      path.join(__dirname, "./images/madison-olling-unsplash.jpg")
+    ),
+    edition: 1,
+    totalCount: 5
+  };
+
+  // The imprint is the hashed proof for our asset
+  console.log("Imprint", await cert.imprint(asset));
+  console.log("Expose", await cert.expose(asset, [["name"], ["image"]]));
+}
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <h1>ERC721 Art Marketplace</h1>
+        <p>
+          In this marketplace you can deploy unique ERC721 art pieces to the
+          blockchain with 0xcert.
+        </p>
+        <div className="assets-container" />
+        <button className="margin-right">Deploy Art Piece</button>
+        <button>Get Art Pieces</button>
       </div>
     );
   }
